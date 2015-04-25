fb709e5832e84d1d22fb49b44165fd1410d984c8
Noah Phillips <nphillips@johnmcneilstudio.com>
2015-04-25 13:10:58 -0700

    adding database modeling

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require('http')
 var ecstatic = require('ecstatic')
 var path = require('path')
 var fs = require('fs')
+
+var level = require('level')
+var db = level('./db')
 var formidable = require('formidable')
 var utils = require('util')
 var server = http.createServer(handler)
@@ -17,13 +20,39 @@ function handler (req, res) {
 	else if(req.method === 'POST') {
 		var form = new formidable.IncomingForm()
 
-		form.uploadDir = './tmp'
+		form.uploadDir = __dirname + '/tmp'
 		
 		form.parse(req, function (err, fields, files) {
-			console.log(utils.inspect({files: files}))
-			form.on('part', function(){console.log('got part')})
-			res.writeHead(304, {'location': 'http://localhost:11001'})
-			res.end()
+
+			// construct meta-data database object
+			var meta = {}
+
+			meta.workTitle 		= fields.workTitle
+			meta.artist 		= fields.artist
+			meta.year 			= fields.year
+			meta.origin 		= fields.origin
+			meta.image 			= {}
+			meta.image.name 	= files.image.name
+			meta.image.path 	= files.image.path
+			meta.image.type 	= files.image.type
+
+			// set key
+			var key = meta.artist + '!' + meta.workTitle
+			
+			db.put(key, JSON.stringify(meta), function (err) {
+				if (err) {
+					console.log(err)
+				}
+				else {
+					res.writeHead(304, {'location': 'http://localhost:11001'})
+				}
+				// read the database and log response
+				db.get(key, function (err, value) {
+				    if (err) return console.log(err) 
+				    console.log(utils.inspect(JSON.parse(value)))
+  				})
+				res.end()
+			})
 		})
 	}
 }
