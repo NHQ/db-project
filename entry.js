400c7356effa17f8d68a2b42c4fb2ce9c3b17ffb
Noah Phillips <nphillips@johnmcneilstudio.com>
2015-07-28 14:42:52 -0700

    [FEATURE] adds window location ID to server and css updates

--- entry.js
+++ entry.js
@@ -1,26 +1,20 @@
 var fs		=require('fs')
+// var template = fs.readFileSync('public/templates/details.html', 'utf8')
 
-var template = fs.readFileSync('public/templates/details.html', 'utf8')
 
-form			= document.querySelector('#input')
-artist		= document.querySelector('[name=artist]')
-workTitle	= document.querySelector('[name=workTitle]')
-year			= document.querySelector('[name=year]')
-origin		= document.querySelector('[name=origin]')
-image			= document.querySelector('[name=image]')
 
-var arr 	= [artist, workTitle, year, origin, image]
+var navInfo     = document.querySelector('.nav').querySelectorAll('.data-link');
+var dataList    = document.querySelector('.data-list-wrap').querySelectorAll('.list');
+var menuToggle  = document.querySelector('#menu-toggle');
+var navWrap     = document.querySelector('.nav-wrapper');
 
-form.addEventListener('submit', function (e) {
-	e.preventDefault()
-	var check = arr.filter(function (el) {
-		return !el.value.length
-	})
-	if (check.length) {
-		check.forEach(function (el) {
-			el.style.border = '2px solid red'
-			console.log(el.getBoundingClientRect())
-		})
-	}
-	console.log(check)
+[].forEach.call(navInfo, function (el, i) {
+  el.addEventListener('click', function () {
+    dataList[i].classList.toggle('show')
+  })
+})
+
+menuToggle.addEventListener('click', function () {
+  this.classList.toggle('on')
+  navWrap.classList.toggle('show')
 })
--- server.js
+++ server.js
@@ -3,57 +3,53 @@ var ecstatic 		= require('ecstatic')
 var path 				= require('path')
 var fs 					= require('fs')
 var level 			= require('level')
-var db 					= level('./db')
 var formidable 	= require('formidable')
 var utils 			= require('util')
+var router 			= require('router')()	
+var db 					= level('./db')
 var server 			= http.createServer(handler)
 
 var static		= ecstatic({root: __dirname + '/public'})
 
+router.post('/', function (req, res) {
+
+	var form = new formidable.IncomingForm()
+
+	form.uploadDir = __dirname + '/tmp'
+	
+	form.parse(req, function (err, fields, files) {
+
+		// set key
+		var key = fields.inventoryNumber
+		
+		db.put(key, JSON.stringify(fields), function (err) {
+			if (err) {
+				console.log(err)
+			}
+			else {
+				res.writeHead(304, {'location': 'http://localhost:11001'})
+			}
+			// read the database and log response
+			res.end()
+		})
+	})
+})
+
+// get ID's
+router.get('/inventory/:id', function (req, res) {
+	var key 	= req.params.id
+	
+	db.get(key, function (err, value) {
+    if (err) return console.log(err) 
+    console.log(utils.inspect(JSON.parse(value)))
+		res.end(value)
+	})
+})
+
 // server handler function
 function handler (req, res) {
 	
-	if(req.method === 'POST') {
-		var form = new formidable.IncomingForm()
-
-		form.uploadDir = __dirname + '/tmp'
-		
-		form.parse(req, function (err, fields, files) {
-
-			// construct meta-data database object
-			var meta = {}
-
-			meta.workTitle 		= fields.workTitle
-			meta.artist 			= fields.artist
-			meta.year 				= fields.year
-			meta.origin 			= fields.origin
-			meta.image 				= {}
-			meta.image.name 	= files.image.name
-			meta.image.path 	= files.image.path
-			meta.image.type 	= files.image.type
-
-			// set key
-			var key = meta.artist + '!' + meta.workTitle
-			
-			db.put(key, JSON.stringify(meta), function (err) {
-				if (err) {
-					console.log(err)
-				}
-				else {
-					res.writeHead(304, {'location': 'http://localhost:11001'})
-				}
-				// read the database and log response
-				db.get(key, function (err, value) {
-				    if (err) return console.log(err) 
-				    console.log(utils.inspect(JSON.parse(value)))
-  				})
-				res.end()
-			})
-		})
-	}
-	else {
-		static(req, res)
-	}
+	router(req, res, function () { static(req, res)})
 }
 
 server.listen(11001, function () {
