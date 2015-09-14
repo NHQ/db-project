cf0e2c65adbebefc84f5b393a901b967b8743012
Noah Phillips <nphillips@johnmcneilstudio.com>
2015-09-14 11:00:50 -0700

    [CONFIG] added test post shell and updated package.json to this end

---
+++ test-post.sh
@@ -0,0 +1,1 @@
+curl --data "inventoryNumber=01&artist=1artist&artistDates=01/01/1111&artistOrigin=1place&workTitle=1title&workYear=1111&workOrigin=1place&workSignature=1signed" localhost:11001/post
